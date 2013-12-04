78d17970462ba008bfda5a68128a58f300f44c43
audibleblink <virtualsnyper@gmail.com>
2013-12-03 19:35:55 -0800

    remove device-conditional styling and moved to media queries in sass file

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,6 @@ Portfolio = {
     this.setListeners()
   },
 
-  formatForMobile: function() {
-    $('nav').css("position", "relative").css("margin", "0");
-    $('body').css("padding", "0");
-    $('.disabled-onepage-scroll').css('overflow-x','hidden')
-    $('.navbar-text').css("margin-left", "25px");
-    $('.main .row .container').css("width","100%").css("margin", "0").css("box-shadow", "0 0 0")
-  },
-
   start: function() {
     $(".main").onepage_scroll({
       animationTime:600,
@@ -23,12 +15,15 @@ Portfolio = {
   setListeners: function(){
     var $img = $('.img-container')
     if(  $(window).width()  <= 800  ) {
-      this.formatForMobile()
+    $img
+      .on('click', function(){ $(this).find('.caption').slideDown('200')})
+      .on('click', function(){ $(this).find('.caption').slideUp('200')})    
+      // $img.toggle(function() { $(this).find(".caption").slideDown('200'); }, function() { $(this).find(".caption").slideUp('200'); })    
     } 
     else {
-    $img
-      .on('mouseenter', function(){ $(this).find('.caption').slideDown('200')})
-      .on('mouseleave', function(){ $(this).find('.caption').slideUp('200')})    
+      $img
+        .on('mouseenter', function(){ $(this).find('.caption').slideDown('200')})
+        .on('mouseleave', function(){ $(this).find('.caption').slideUp('200')})    
     }
   }
 }
